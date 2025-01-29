a7f75eb7847227ceacb641d138f71cec05582969
Daisi Alves <daisiv.alves@gmail.com>
2025-01-28 23:00:47 -0300

    Exercício 7

---
+++ vetores/src/application/Ex07Vet.java
@@ -0,0 +1,37 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex07Vet {
+    
+    //maior posição
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Quantos números você vai digitar ? ");
+        int n = sc.nextInt();
+        int [] vect = new int [n];
+
+        for (int i =0; i<vect.length; i++) {
+            System.out.print("Digite um número: ");
+            vect [i] = sc.nextInt(); 
+        }
+        int maior= 0;
+        int position = 0;
+        for (int i=0; i<vect.length; i++) {
+            if (vect [i] > maior) {
+                maior = vect[i];
+                position = i;
+            }
+        }
+        System.out.println("Maior número: " + maior);
+        System.out.print("Posição do maior: " + position);
+
+
+
+
+    }
+}
