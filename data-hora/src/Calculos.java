fa5869b0e145b8c02e15d35d62eb6db7bc977e09
Daisi Alves <daisiv.alves@gmail.com>
2025-02-06 21:55:07 -0300

    Contagem de dias

--- data-hora/src/Calculos.java
+++ data-hora/src/Calculos.java
@@ -1,6 +1,8 @@
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class Calculos {
     public static void main(String[] args) {
@@ -9,10 +11,21 @@ public class Calculos {
         LocalDateTime d05 = LocalDateTime.parse("2022-07-20t01:30:26");
         Instant d06 = Instant.parse("2022-07-20t01:30:26Z");
 
-        LocalDate pastWeekLocalDate = d04.minusDays(7);
-        LocalDate nextWeekLocalDate = d04.plusDays(7);
+        LocalDateTime pastWeekLocalDate = d05.minusDays(7);
+        LocalDateTime nextWeekLocalDate = d05.plusDays(7);
 
         System.out.println("pastWeekLocalDate = " + pastWeekLocalDate);
         System.out.println("nextWeekLocalDate = " + nextWeekLocalDate);
+
+        Instant pastWeekInstant = d06.minus(7, ChronoUnit.DAYS);
+        System.out.println("pastWeekInstant = " + pastWeekInstant);
+
+        Instant nextWeekInstant = d06.plus(7, ChronoUnit.DAYS);
+
+        System.out.println("nextWeekInstant = " + nextWeekInstant);
+
+        Duration t1 = Duration.between(pastWeekLocalDate, d05);
+
+        System.out.println("Duração entre um e outro: " + t1.toDays());
     }
 }
