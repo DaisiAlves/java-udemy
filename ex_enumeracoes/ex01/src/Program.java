68cdf53e11ebaee7a7f35ec2b462db22d0c112d3
Daisi Alves <daisiv.alves@gmail.com>
2025-02-15 23:12:51 -0300

    Atualizações

--- ex_enumeracoes/ex01/src/Program.java
+++ ex_enumeracoes/ex01/src/Program.java
@@ -1,13 +1,28 @@
 //Exercíco 1 de enumerações
 
+import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Department;
+import entities.Worker;
 
 public class Program {
     public static void main(String[] args) {
-        
+
+
+      Locale.setDefault(Locale.US);  
       Scanner sc = new Scanner (System.in);
 
-      int a = sc.nextInt
+      System.out.println("Enter department's name: ");
+      String departmentName = sc.nextLine();
+      System.out.println("Enter worker data:");
+      System.out.println("Name: ");
+      String workerName = sc.nextLine();
+      System.out.println("Level: ");
+      String workerLevel = sc.nextLine();
+      System.out.println("Base salary: ");
+      double baseSalary = sc.nextDouble();
+      Worker worker = new Worker (workerName, WorkerLevel.valueOf(workerLevel), baseSalary, new Department(departmentName));
 
     }
 }
