a797e71aca4441fc5659ff0d29432fe1e9b0616b
Daisi Alves <daisiv.alves@gmail.com>
2025-02-17 22:30:56 -0300

    Create class comment and post

---
+++ ex_enumeracoes/ex02/src/entities/Post.java
@@ -0,0 +1,64 @@
+package entities;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Post {
+
+    private LocalDate moment; 
+    private String title;
+    private String content;
+
+    private List<Comment> comments = new ArrayList <>();
+
+    public Post () {
+
+    }
+
+    public Post(LocalDate moment, String title, String content) {
+        this.moment = moment;
+        this.title = title;
+        this.content = content;
+    }
+
+    public LocalDate getMoment() {
+        return moment;
+    }
+
+    public void setMoment(LocalDate moment) {
+        this.moment = moment;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    // Não esquecer que não podemos ter o método set na lista, pois isso permitira substituir a lista 
+
+    public void addComment(Comment comment) {
+        comments.add(comment);
+    }//Adicionar comentário
+
+    public void removeComment(Comment comment) {
+        comments.remove(comment);
+    }//Remover comentário
+
+    
+}
