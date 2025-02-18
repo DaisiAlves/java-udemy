b6f2f7a58bd507fa4470dcf85ecc8290a46016ff
Daisi Alves <daisiv.alves@gmail.com>
2025-02-17 21:30:08 -0300

    Trabalhando com API de data e hora

--- ex_enumeracoes/ex01/src/Program.java
+++ ex_enumeracoes/ex01/src/Program.java
@@ -1,19 +1,25 @@
 //Exercíco 1 de enumerações
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Department;
+import entities.HourContract;
 import entities.Worker;
 import enums.WorkerLevel;
 
 
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
 
       Locale.setDefault(Locale.US);  
       Scanner sc = new Scanner (System.in);
+      SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
       System.out.println("Enter department's name: ");
       String departmentName = sc.nextLine();
@@ -33,6 +39,14 @@ public class Program {
       for (int i =1; i<=n; i++){
         System.out.println("Enter contract #" + i + "data: ");
         System.out.print("Date (DD/MM/YYYY): ");
+        String data1 = sc.next();
+        LocalDate contractDate = LocalDate.parse(data1, DateTimeFormatter.ofPattern("dd/MM/yyyy")); 
+        System.out.print("Value per hour: ");
+        double valuePerHour = sc.nextDouble();
+        System.out.print("Duration (hours): ");
+        int hours = sc.nextInt();
+        HourContract contract = new HourContract(contractDate, valuePerHour, hours);
+        worker.addContract(contract); //Isso faz com que meu contrato seja associado a um trabalhador 
       }
     }
 }
