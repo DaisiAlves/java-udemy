55ed3c45cea0cb5f5b5ee72fee0194bfd5700078
Daisi Alves <daisiv.alves@gmail.com>
2025-02-26 22:29:13 -0300

    Inclusão dos últimos comentários

--- ex_enumeracoes/ex02/src/application/Program.java
+++ ex_enumeracoes/ex02/src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import entities.Comment;
@@ -14,11 +14,21 @@ public class Program {
         Comment c1 = new Comment("Have a nice trip");
         Comment c2 = new Comment("Wow that's awesome!");
 
-        LocalDate moment = LocalDate.parse("21/06/2018 13:05:44", dtf);
+        LocalDateTime moment = LocalDateTime.parse("21/06/2018 13:05:44", dtf);
         Post p1 = new Post(moment, "Traveling to New Zealand", "I'm going to visit this wonderfil country!", 12);
-        
+
         p1.addComment(c1);
         p1.addComment(c2);
+
+        Comment c3 = new Comment("Good night");
+		Comment c4 = new Comment("May the Force be with you");
+        LocalDateTime moment2 = LocalDateTime.parse("28/07/2018 23:14:19", dtf);
+		Post p2 = new Post(moment2, "Good night guys", "See you tomorrow", 5);
+		p2.addComment(c3);
+		p2.addComment(c4);
+		
+		System.out.println(p1);
+		System.out.println(p2);
     }
-    
+
 }
--- ex_enumeracoes/ex02/src/entities/Post.java
+++ ex_enumeracoes/ex02/src/entities/Post.java
@@ -1,38 +1,37 @@
 package entities;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Post {
 
-    private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern ("dd/MM/yyyy HH:mm:ss");
+    private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
 
-    private LocalDate moment; 
+    private LocalDateTime moment;
     private String title;
     private String content;
     private Integer likes;
 
-    
-    private List<Comment> comments = new ArrayList <>();
+    private List<Comment> comments = new ArrayList<>();
 
-    public Post () {
+    public Post() {
 
     }
 
-    public Post(LocalDate moment, String title, String content, Integer likes) {
+    public Post(LocalDateTime moment, String title, String content, Integer likes) {
         this.moment = moment;
         this.title = title;
         this.content = content;
-        this.likes= likes;
+        this.likes = likes;
     }
 
-    public LocalDate getMoment() {
+    public LocalDateTime getMoment() {
         return moment;
     }
 
-    public void setMoment(LocalDate moment) {
+    public void setMoment(LocalDateTime moment) {
         this.moment = moment;
     }
 
@@ -60,35 +59,33 @@ public class Post {
         this.likes = likes;
     }
 
-
     public List<Comment> getComments() {
         return comments;
     }
 
-    // Não esquecer que não podemos ter o método set na lista, pois isso permitira substituir a lista, fazendo somente o metodo add e remove
+    // Não esquecer que não podemos ter o método set na lista, pois isso permitira
+    // substituir a lista, fazendo somente o metodo add e remove
 
     public void addComment(Comment comment) {
         comments.add(comment);
-    }//Adicionar comentário
+    }// Adicionar comentário
 
     public void removeComment(Comment comment) {
         comments.remove(comment);
     }
 
-   
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append(title + "\n");//append significa acrescentar no final 
+        sb.append(title + "\n");// append significa acrescentar no final
         sb.append(likes);
         sb.append("Likes = ");
-        sb.append(dtf.format(moment)+ "\n");
+        sb.append(dtf.format(moment) + "\n");
         sb.append(content + "\n");
         sb.append("Comments:\n");
         for (Comment c : comments) {
-            sb.append(c.getText());
+            sb.append(c.getText() + "\n");
         }
         return sb.toString();
     }
 
-    
 }
