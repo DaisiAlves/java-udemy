3db9a363a1bfbbf6ba20b9f74c87e1607e615311
Daisi Alves <daisiv.alves@gmail.com>
2025-03-04 00:13:33 -0300

    classes product and OrderItem

---
+++ ex_enumeracoes/ex03/src/entities/OrderStatus.java
@@ -0,0 +1,11 @@
+package entities;
+
+//tipo enumerado que guarda um valor quepoder ser uma lista pré definida de constantes 
+
+public enum OrderStatus {
+
+    PENDING_PAYMENT,
+    PROCESSSING,
+    SHIPPED,
+    DELIVERED,
+}
