554c6c09d5bca218a9af12113303fed8a9ce8de8
Daisi Alves <daisiv.alves@gmail.com>
2025-03-07 23:52:50 -0300

    palavra "final"

---
+++ herança/ex01/src/entities/SavingsAccountPlus.java
@@ -0,0 +1,7 @@
+package entities;
+
+public class SavingsAccountPlus extends SavingsAccount {
+ // O compilador não deixa herdar de SavingsAccount, uma vez que 
+ 
+ //A mesma coisa pode acontecer com o método. 
+}
