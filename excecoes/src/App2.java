b83e72ff11c9b3f46147175039f83f7984ad634c
Daisi Alves <daisiv.alves@gmail.com>
2026-07-01 19:42:13 +0100

    Stack Trace

--- excecoes/src/App2.java
+++ excecoes/src/App2.java
@@ -1,9 +1,24 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class App {
-    public static void main(String[] args) {
+public class App2 {
 
+    public static void main(String[] args) {
+        method1();
+    }
+
+    
+
+    public static void method1(){
+        System.out.println("***METHOD 1 START***");
+        method2();
+        System.out.println("***METHOD 1 END***");
+    }
+    
+    
+    public static void method2() {
+
+        System.out.println("***METHOD 2 START***");
         Scanner sc = new Scanner(System.in);
 
         try {
@@ -15,16 +30,17 @@ public class App {
 
         catch (ArrayIndexOutOfBoundsException e) { // Criei um catch para tratar a exceção de ArrayIndexOutOfBoundsException, esse tipo de exceção é lançada quando o usuário tenta acessar uma posição inválida do vetor.
             System.out.println("Invalid position!"); // Imprimi uma mensagem de erro caso o usuário tente acessar uma posição inválida.
+            e.printStackTrace(); // Imprimi o rastreamento da pilha de exceções, isso é útil para depuração, pois mostra onde a exceção ocorreu no código. Vai mostrar a pilha de chamadas de métodos que levaram à exceção, incluindo o nome do arquivo e o número da linha onde a exceção foi lançada, antes de encerrar o programa.
         }
 
         catch(InputMismatchException e) { //Essa outra exceção é lançada quando o usuário digita um valor que não é um número inteiro, por exemplo, se ele digitar uma letra ou um número decimal.
         System.out.println("Input error");
+        e.printStackTrace();
         }
 
         System.out.println("End of program!"); // Imprimi uma mensagem de fim de programa.
 
         sc.close();
-        
-
     }
+    
 }
